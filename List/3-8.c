09fabde7ca7e14a1a74de7eb331c757371a2a484
shotaro-ada <syouta2223505@gmail.com>
2023-03-05 10:23:36 +0900

    d

--- List/3-8.c
+++ List/3-8.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void fill(int a[2][3], int v)
+void fill(int (*a)[3], int n, int v)
 {
     int i, j;
 
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < n; i++)
     {
         for (j = 0; j < 3; j++)
         {
@@ -17,17 +17,30 @@ int main(void)
 {
     int i, j, no;
     int x[2][3] = {0};
+    int y[4][3] = {0};
 
-    printf("全構成要素に代入する値 : ");
+    printf("全構成要素に代入する値: ");
     scanf("%d", &no);
 
-    fill(x, no);
+    fill(x, 2, no);
+    fill(y, 4, no);
 
+    puts("--- x ---");
     for (i = 0; i < 2; i++)
     {
         for (j = 0; j < 3; j++)
         {
             printf("%3d", x[i][j]);
+        }
+        putchar('\n');
+    }
+
+    puts("--- y ---");
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%3d", y[i][j]);
         }
         putchar('\n');
     }
